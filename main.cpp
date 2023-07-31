795c7ef0d33855184319eb3877c1e5d5feb06e84
CoderColby <97490678+CoderColby@users.noreply.github.com>
2023-07-31 05:15:12 +0000

    Created comments that describe the code and personal header for assignment

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
+/*
+Colby Anderson
+CIS 1202 800
+July 30 2023
+*/
+
+
 #include <iostream>
 using namespace std;
 
+// Thrown if starting character is not a letter
 class invalidCharacterException {
   private:
-    char invalidChar;
+    char invalidChar; // Character that caused the exception
   public:
-    invalidCharacterException(char c) {
+    invalidCharacterException(char c) { // Accepts the conditions that caused the exception
       invalidChar = c;
     }
     char getChar() {
@@ -13,12 +21,13 @@ class invalidCharacterException {
     }
 };
 
+// Thrown if ending character is not a letter or if the case changed
 class invalidRangeException {
   private:
-    char invalidChar;
-    int invalidRange;
+    char invalidChar; // Starting character
+    int invalidRange; // Range that caused the exception
   public:
-    invalidRangeException(char c, int i) {
+    invalidRangeException(char c, int i) { // Accepts the conditions that caused the exception
       invalidChar = c;
       invalidRange = i;
     }
@@ -31,32 +40,37 @@ class invalidRangeException {
 };
 
 
+// Given from instructions
 char character(char start, int offset);
 
 
 int main() {
-  char start = 'A';
-  int offset = 32;
+  // Starting conditions
+  char start = 'A'; // Change me
+  int offset = 32; // Change me
 
+  // Try to call character function and catch exceptions if unsuccessful
   try {
     cout << start << "+" << offset << "=" << character(start, offset);
-  } catch (invalidCharacterException e) {
+  } catch (invalidCharacterException e) { // Starting character was not alphabetic
     cout << "ERROR: The character " << e.getChar() << " is not a letter.";
-  } catch (invalidRangeException e) {
+  } catch (invalidRangeException e) { // Ending character was not alphabetic or the range changed the case of the character
     cout << "ERROR: The range " << e.getRange() << " is not valid with character " << e.getChar() << ".";
   }
 }
 
 
+// Moves the starting character by the distance in offset and throws an exception if 'start' or 'end' is not valid
 char character(char start, int offset) {
 
-  if (!isalpha(start))
+  if (!isalpha(start)) // If 'start' is not a letter
     throw invalidCharacterException(start);
 
-  char end = start + offset;
+  char end = start + offset; // Change character according to ASCII values
 
-  if (!isalpha(end) || (start - 91) * (end - 91) < 0)
+  if (!isalpha(end) || (start - 91) * (end - 91) < 0) // If 'end' is not a letter or the character '[' (91 in ASCII) is between 'start' and 'end' which signifies that the character has changed case 
     throw invalidRangeException(start, offset);
 
+  // Return character if no exceptions occurred
   return end;
 }
