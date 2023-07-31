cb42b3684abfbdaad3d7b9c9c3ac8c92b7cedc3f
CoderColby <97490678+CoderColby@users.noreply.github.com>
2023-07-31 04:13:31 +0000

    Created a portion of the character function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+using namespace std;
+
+char character(char start, int offset);
 
 int main() {
   std::cout << "Hello World!\n";
+}
+
+
+char character(char start, int offset) {
+  string charException = "ERROR: Invalid character\n";
+  string rangeException = "ERROR: Invalid range\n";
+
+  if (!isalpha(start))
+    throw charException;
 }
